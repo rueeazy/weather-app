74695e6275ade9f07d35c4ab66f85c24e4869944
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-02 17:37:47 -0700

    Add files and finish header styling

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+        <input class="weather-input" type="text" placeholder="Enter a city"></input> 
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div className="main-content">
+    </div>
+  );
+}
+
+export default MainContent;
