563dacacbcf0a0ae4cf757f5a458ba33cbcab43b
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-05 12:26:16 -0700

    Updates

---
+++ precache-manifest.1e6fb2c2d364109477db65b86a74c253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5497931b3fa6eb33d325691309305a3c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "43dfd387618b41e22124",
+    "url": "/weather-app/static/css/main.1c5064f0.chunk.css"
+  },
+  {
+    "revision": "ea1f13a88e799ce3ff38",
+    "url": "/weather-app/static/js/2.0309804a.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/weather-app/static/js/2.0309804a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dfd387618b41e22124",
+    "url": "/weather-app/static/js/main.1685503e.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "8c1eca0386d7bc851fb63d6c26973ad8",
+    "url": "/weather-app/static/media/background.8c1eca03.jpg"
+  }
+]);
